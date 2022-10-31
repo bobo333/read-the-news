4b183332182db624f1e5859fae643a7c799d2f3c
Steven Cipriano <redsox333@gmail.com>
2022-10-31 01:45:17 -0700

    updates
    - change examples to newer files
    - minor fixes for by-lines and dates
    - make article url a cli argument

--- article.py
+++ article.py
@@ -1,25 +1,43 @@
+from argparse import ArgumentParser
+
 import textwrap
 from bs4 import BeautifulSoup
+import requests
 
 def nice_print(input_text):
     print('\n'.join(textwrap.wrap(input_text)), '\n')
 
-with open('examples/sdut_article.html') as f:
-    soup = BeautifulSoup(f.read(), 'html.parser')
-
-sections = []
-
-sections.append(soup.title.text)
-byline = soup.find(class_='byline')
-sections.append(byline.find(class_='authors').text)
-sections.append("{} {}".format(byline.find(class_='published-day').text, byline.find(class_='published-time').text))
-paragraphs = soup.find(class_='rich-text-article-body-content').find_all('p')
+headers = {
+    'User-Agent': 'read-the-news'
+}
 
 
-for p in paragraphs:
-    if not p.text:
-        continue
-    sections.append('\n'.join(textwrap.wrap(p.text)))
+def main():
+    parser = ArgumentParser(description="Get the news, so you can read it",
+        epilog="knowledge is power")
+    parser.add_argument("url")
+    args = parser.parse_args()
 
-for s in sections:
-    nice_print(s)
+    page = requests.get(args.url, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    sections = []
+
+    sections.append(soup.title.text)
+    byline = soup.find(class_='byline')
+    sections.append(byline.find(class_='authors').text)
+    sections.append("{}".format(byline.find(class_='published-date').text))
+    paragraphs = soup.find(class_='rich-text-article-body-content').find_all('p')
+
+
+    for p in paragraphs:
+        if not p.text:
+            continue
+        sections.append('\n'.join(textwrap.wrap(p.text)))
+
+    for s in sections:
+        nice_print(s)
+
+
+if __name__ == "__main__":
+    main()
